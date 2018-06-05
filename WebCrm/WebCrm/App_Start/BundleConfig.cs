38bf35c87b031da1fc26a225e99dbb5320a5fe94
Amel Masinovic <amel_masinovic@hotmail.com>
2018-06-05 13:31:49 +0200

    add bootstrap to bundle

--- WebCrm/WebCrm/App_Start/BundleConfig.cs
+++ WebCrm/WebCrm/App_Start/BundleConfig.cs
@@ -21,11 +21,13 @@ namespace WebCrm
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
 								"~/Scripts/bootstrap.js",
-								"~/Scripts/respond.js"));
+								"~/Scripts/respond.js",
+								"~/Scripts/bootstrap.min.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-								"~/Content/bootstrap.css",
-								"~/Content/site.css"));
+				"~/Content/bootstrap.min.css",
+				"~/Content/bootstrap-responsive.min.css",
+				"~/Content/site.css"));
 		}
 	}
 }
